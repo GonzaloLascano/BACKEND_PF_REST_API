5f672f18c974c36bc573e7931614bd2ef5614d0e
Gonzalo Ivan Puniales Lascano <lascano.gonzalo.ivan@gmail.com>
2022-07-14 18:50:15 -0300

    segundo commit para la segunda entrega del proyecto final

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Hasta ahora se incorporaron endpoints para 'api/productos' y 'api/carrito' segun
 Se reestructuro toda la arquitectura del proyecto, y se crearon controladores y modelos para los distintos tipos de almacenamiento (archivos, base de datos) para los distintos tipos de objetos (productos y carritos) 
 
 ### Puntos de dificultad o a mejorar ###
-La arquitectura de los modulos esta en etapa experimental. Hay que refinar las validaciones de los modelos de mongodb y atrapar errores en caso de escritura en los controladores de mongodb. Se puede crear un switch para no tener que comentar los endpoints utilizados y sus metodos.
+La arquitectura de los modulos esta en etapa experimental. Hay que refinar las validaciones de los modelos de mongodb. Se puede crear un switch para no tener que comentar los endpoints utilizados y sus metodos en routes.
 
 > Deployed on Glitch: [project](https://glitch.com/edit/#!/twilight-lizard-hook/) [live site](https://twilight-lizard-hook.glitch.me/)
 
--- controllers/chartMongo.js
+++ controllers/chartMongo.js
@@ -4,42 +4,73 @@ const ChartMongoModel = require('../models/db/chartsMongo')
 const { ProdMongoModel } = require('../models/db/productsMongo')
 
 const createChartM = async (req, res) => {
-    const idCount = await ChartMongoModel.findOne({}, {chartId:1}).sort({chartId: -1});
-    console.log(idCount);  
-    const response = await ChartMongoModel.create({chartId: idCount == null ? 1 : idCount.chartId + 1, time: Date.now(), prods: []})
-    res.json({mensaje: `New empty chart created! id: ${response.chartId}`})
+    try {
+        const idCount = await ChartMongoModel.findOne({}, {chartId:1}).sort({chartId: -1});
+        console.log(idCount);  
+        const response = await ChartMongoModel.create({chartId: idCount == null ? 1 : idCount.chartId + 1, time: Date.now(), prods: []})
+        res.json({mensaje: `New empty chart created! id: ${response.chartId}`})
+    } catch (err) {
+        console.log(err);
+        res.json({mesaje:'could not create chart'});
+    }
+    
 }
 
 const deleteChartM = async (req, res) => {
-    const response = await ChartMongoModel.deleteOne({chartId: req.params.id})
-    console.log(response)
-    res.json({message: 'chart deleted successfully'})  
-}
-
-const getChartProductsM =  async (req, res) => {     
-    const response = await ChartMongoModel.findOne({chartId: req.params.id})
-    console.log(response.prods)                      
-    if (response.prods.length > 0) {
-        res.json(response.prods)
+    try{
+        const response = await ChartMongoModel.deleteOne({chartId: req.params.id})
+        console.log(response)
+        res.json({message: 'chart deleted successfully'})
+    } catch(err) {
+        console.log(err);
+        res.json({mesaje:'could not delete requested chart'});    
     }
-    else {res.json('chart is empty')}
+      
 }
 
-const addProductToChartM = async (req, res) => {                         
-    const prodToAdd = await ProdMongoModel.findOne({prodId: req.params.id_prod})
-    const selectedChart = await ChartMongoModel.findOneAndUpdate({chartId: req.params.id}, {$push: {prods: prodToAdd}})
-    res.json({message: 'added ok'})
+const getChartProductsM =  async (req, res) => {
+    try{
+        const response = await ChartMongoModel.findOne({chartId: req.params.id})
+        console.log(response.prods)                      
+        if (response.prods.length > 0) {
+            res.json(response.prods)
+        }
+        else {res.json('chart is empty')}
+
+    } catch(err) {
+        console.log(err);
+        res.json({mesaje:'could not read database'});
+    }    
+    
+}
+
+const addProductToChartM = async (req, res) => {  
+    try{
+        const prodToAdd = await ProdMongoModel.findOne({prodId: req.params.id_prod})
+        const selectedChart = await ChartMongoModel.findOneAndUpdate({chartId: req.params.id}, {$push: {prods: prodToAdd}})
+        res.json({message: 'added ok'})
+    } catch(err) {
+        console.log(err);
+        res.json({mesaje:'could not add product to recuested chart'});
+    }                      
+    
 }
 
 const deleteChartProductM =  async (req, res) => {
-    const selectedChart = await ChartMongoModel.findOneAndUpdate({chartId: req.params.id}, 
-        { $pull: 
-            {prods: 
-                { prodId: req.params.id_prod} 
+    try {
+        const selectedChart = await ChartMongoModel.findOneAndUpdate({chartId: req.params.id}, 
+            { $pull: 
+                {prods: 
+                    { prodId: req.params.id_prod} 
+                }
             }
-        }
-    )
-    res.json({message:"product deleted successfully from chart"})
+        )
+        res.json({message:"product deleted successfully from chart"})
+    } catch(err){
+        console.log(err);
+        res.json({mesaje:'could not delete requested product from chart'});
+    }
+    
 }
 
 module.exports = {createChartM, deleteChartM, getChartProductsM, addProductToChartM, deleteChartProductM}
